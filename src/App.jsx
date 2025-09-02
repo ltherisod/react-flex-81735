b33b7e59c4d9fb62a62a83ef7db504bcc0a2ceb9
ltherisod <lauratherisod@gmail.com>
2025-09-02 18:26:37 -0300

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,17 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ItemCount from './components/ItemCount'
 import ItemListContainer from './components/ItemListContainer'
-import NavBar from './components/NavBar'
-import Button from "./examples/Button"
 import NavBarRB from './components/NavBarRB';
 function App() {
-  const saludar = ()=>{
-    alert('Hola')
-  }
-  const despedir = ()=>{
-    console.log('chau chau 🙌')
-  }
+
 
   console.log('App')
 
   return (
     <> 
-    {/* <NavBar/> */}
     <NavBarRB/>
     <ItemListContainer mensaje="Bienvenidos a CoderShop!"/>
-    {/* <p>Holis!</p>
-    <p>Holis!</p>
-    <Button text='Hola' clickHandler={saludar} color="yellow"/>
-    <Button text='Chau' clickHandler={despedir} color="green"/> */}
     <ItemCount/>
-    {/* <Button  clickHandler={saludar} color="yellow">Hola!</Button>
-    <Button  clickHandler={despedir} color="green">Chau!</Button>
-    <Button  clickHandler={despedir} color="green"><div><p>🤭</p></div><div><span>lala@lala.ccom</span></div></Button> */}
     </>
   )
 }
